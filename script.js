e93ef2fd0312cd72e8742185d8924d2140aa9fa9
ayatramy <82686850+ayatramy@users.noreply.github.com>
2026-03-07 01:54:09 +0200

    Change message on button click in script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementById("btn").addEventListener("click", () => {
-  document.getElementById("msg").innerText = "✅ JavaScript is working!";
+  document.getElementById("msg").innerText = " Hello from JavaScript! OK";
+
 });
